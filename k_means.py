b15dd84cbe17166170d9260a901cf77d1a47ae62
StevenSYT <syt403601928@gmail.com>
2017-12-02 16:50:07 -0600

    Done!!

--- k_means.py
+++ k_means.py
@@ -12,7 +12,7 @@ def clusterize(row, clusters):
       if (distance < minDistance):
          minDistance = distance
          targetIndex = index
-   print("this point goes to the cluster", targetIndex, "with distance ", minDistance)
+   # print("this point goes to the cluster", targetIndex, "with distance ", minDistance)
    clusters[targetIndex] = pd.concat([clusters[targetIndex], row])
 
 def compareCenters(centroids, center):
@@ -22,44 +22,47 @@ def compareCenters(centroids, center):
    return True
 
 def kMeans(data, centroids, clusters):
+   print("Begin Clustering...")
    iteration = 1
    center = []
 
    while(iteration <= maxIteration):
+      print("iteration", iteration,"...")
       if(center!= []):
          clusters = center
          centroids = center
          center = []
       for index, row in data.iterrows():
-         print("working on point ", index, "...")
+         # print("working on point ", index, "...")
          clusterize(data.iloc[[index], :], clusters)
 
       for index, cluster in enumerate(clusters):
          if (cluster.mean(axis = 0).to_frame().T.equals(cluster.iloc[[0], :])):
-            pass
-         else:
-
-            center.append(cluster.mean(axis = 0).to_frame().T)
+            print("centroid", index, "converges")
+         center.append(cluster.mean(axis = 0).to_frame().T)
       if(compareCenters(centroids, center)):
          break
       iteration += 1
+   print("Clustering finishes!")
    return clusters
 
 def SSE(clusters):
+   print("computing SSE... ")
    sseList = []
    for index, cluster in enumerate(clusters):
       sse = 0
       mean = cluster.mean(axis = 0)
       meanX = mean['x']
       meanY = mean['y']
-      print("cluster ", index)
-      print(cluster)
+      # print("cluster ", index)
+      # print(cluster)
       for i, row in cluster.iterrows():
          sse += ((meanX - row['x']) ** 2 + (meanY - row['y']) ** 2)
       sseList.append(sse)
    return sum(sseList)
 
 def outPut(outPath, clusters, sseResult):
+   print("exporting the clusters to file'",outPath,"'" )
    outFile = open(outPath,"w+")
    for index, cluster in enumerate(clusters):
       outFile.write(str(index)+"\t")
@@ -70,10 +73,12 @@ def outPut(outPath, clusters, sseResult):
       outFile.write("\n")
    outFile.write("SSE = ")
    outFile.write(str(sseResult))
+   print("Done!!")
 # iloc[[index],[col_num]] keeps dataframe type
 # print(pd.concat([data.iloc[[0], :], data.iloc[[1], :]]))
 
-clusterNum =  sys.argv[1] if len(sys.argv)>1 else 5
+clusterNum =  int(sys.argv[1]) if len(sys.argv)>1 else 5
+# print(clusterNum)
 inPath = sys.argv[2] if len(sys.argv) > 2 else "../data1.txt"
 outPath = sys.argv[3] if len(sys.argv) > 3 else "result.txt"
 data = pd.read_csv(inPath, sep=("\t"))
@@ -85,12 +90,13 @@ maxIteration = 25
 centerIndices = random.sample(range(len(data)), clusterNum)
 centroids = []
 clusters = []
-print(data.mean(axis = 0).to_frame().T)
+# print(data.mean(axis = 0).to_frame().T)
 
 for i, item in enumerate(centerIndices):
    clusters.append(data.iloc[[item],:])
    centroids.append(data.iloc[[item],:])
    # print(clusters[i])
+print("You decided to use",len(clusters),"clusters")
 clusters = kMeans(data, centroids, clusters)
 # print("length of clusters is ",len(clusters))
 sseResult = SSE(clusters)
